dbd5345a379aa1398f6613b4f3cc8d9701431baf
Alaa Adel <alaaadelahmed50@gmail.com>
2023-09-19 19:40:46 +0300

    add sliders

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-var swiper = new Swiper(".card", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+const initSlider = () => {
+  const cardList = document.querySelector(".courses-container");
+  const slideButtons = document.querySelectorAll(".slide-btn");
+
+  // Slide cards according to the slide button clicks
+  slideButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "prev-slide" ? -1 : 1;
+      const scrollAmount = cardList.clientWidth * direction;
+      cardList.scrollBy({ scrollAmount, behavior: "smooth" });
+      console.log(999);
+      console.log(cardList);
+    });
+  });
+};
+window.addEventListener("load", initSlider);
