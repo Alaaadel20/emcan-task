4022119736a84bc695f5e073142e93ae2b56d682
Alaa Adel <alaaadelahmed50@gmail.com>
2023-10-01 12:04:51 +0300

    res

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-const initSlider = () => {
-  const cardList = document.querySelector(".courses-container");
-  const slideButtons = document.querySelectorAll(".slide-btn");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-  // Slide cards according to the slide button clicks
-  slideButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const direction = button.id === "prev-slide" ? -1 : 1;
-      const scrollAmount = cardList.clientWidth * direction;
-      cardList.scrollBy({ scrollAmount, behavior: "smooth" });
-      console.log(999);
-      console.log(cardList);
-    });
-  });
-};
-window.addEventListener("load", initSlider);
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
