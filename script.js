20931773832c9a8646bae30e90e91bb242b48a4c
Alaa Adel <alaaadelahmed50@gmail.com>
2023-10-05 15:50:54 +0300

    res10

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+const btnNavEl = document.querySelector(".navbar-toggler");
+const btnCloseEl = document.querySelector(".close-icon");
+const headerEl = document.querySelector(".sidebar");
 
 btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
+  headerEl.classList.toggle("nav-openz");
+});
+
+btnCloseEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-openz");
 });
